e73c4bcf4149fd0cbab6b826ffd988d7b945589f
Roozki <zawadzkirowan@gmail.com>
2026-03-18 09:46:53 -0700

    quicksave

--- frontend/server.h
+++ frontend/server.h
@@ -2,28 +2,10 @@
 // Main header 
 // #include "database.h"
 #include "storage.h"
+#include "datatypes.h"
 
-#include <string>
-#include <cstdint>
-#include <unordered_map>
 
-enum class ItemStatus {
-    unregistered, // Item not inited
-    in_stock, // Item is in the kitchen
-    out_of_stock // Item is out of stock or will be soon
-};
-
-struct Item {
-    // std::string name = "UNAMED_ITEM";
-    uint32_t card_uid = 0;
-    ItemStatus status = ItemStatus::unregistered;
-};
-
-struct User {
-    // std::string username = "UNREGISTERED_USER";
-    std::unordered_map<std::string, Item> item_map = {};
-};
-
+using user_map_t = std::unordered_map<std::string, User>;
 
 class App {
 public:
@@ -34,7 +16,7 @@ public:
 
 private:
     User admin_user;
-    std::unordered_map<std::string, User> user_map = {};
+    user_map_t user_map = {};
 
 
 };
--- frontend/storage.h
+++ frontend/storage.h
@@ -1,1 +1,12 @@
 #pragma once
+#include <fstream>
+#include "datatypes.h"
+
+class NonVolatileMemory {
+public:
+    NonVolatileMemory(user_map_t& user_map) : user_map(user_map) {
+
+    }
+private:
+user_map_t& user_map;
+};
